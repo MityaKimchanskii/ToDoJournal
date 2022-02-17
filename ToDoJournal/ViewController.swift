879c3438032bae04e256b3607abf32adab8e8339
Dmitrii Kim <mitya_kim@icloud.com>
2022-02-16 22:38:42 -0800

    Initial Commit

---
+++ ToDoJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoJournal
+//
+//  Created by Mitya Kim on 2/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
